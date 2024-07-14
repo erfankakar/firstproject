e7f39f3c844e667fb6b2d011dff27a2e5538de47
erfankakar <orfankakar808@gmail.com>
2024-07-14 06:54:18 -0700

    first project created

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel</title>
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+</head>
+<body>
+    <button class="btn btn-primary"></button>
+    <h1>laravel</h1>
+</body>
+</html>
